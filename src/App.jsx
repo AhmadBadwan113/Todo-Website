4d0b0f3ee9468bee485d32ae294a0f9d486941d8
Ahmad <ahmadzeyad80@outlook.com>
2024-10-10 02:20:39 +0400

    reverting stuff

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/App.jsx" element={<Home />} /> */}
+        <Route path="/App.jsx" element={<Home />} />
         <Route path='/Contact' element={<Contact />} />
         <Route path='/About' element={<About />} />
       </Routes>
